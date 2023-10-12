40f7dcec8b808a8a698b24fa79e96690974b0b39
Ladro <kennysp2006@mail.ru>
2023-10-12 21:15:25 +0300

    1

--- djangopp/News/urls.py
+++ djangopp/News/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 
-from News.views import HomeNews, NewsByCategory, ViewNews, AddNews, test
+from News.views import HomeNews, NewsByCategory, ViewNews, AddNews, register, user_login, user_logout
 # from News.views import index, get_category, view_news, add_news
 
 urlpatterns = [
@@ -13,7 +13,10 @@ urlpatterns = [
     path('category/<int:category_id>', NewsByCategory.as_view(), name='Category'),
     path('news/<int:pk>', ViewNews.as_view(), name='View_news'),
     path('news/add_news', AddNews.as_view(), name='Add_news'),
-    path('test/', test, name='Test'),
+    path('register/', register, name='Register'),
+    path('login/', user_login, name='Login'),
+    path('logout/', user_logout, name='Logout'),
+
 ]
 
 
